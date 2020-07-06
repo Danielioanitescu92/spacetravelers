5f3accf70ae3e3a92e7e37c9bbd6e9830f77f6b5
Daniel <daniel.ioanitescu@gmail.com>
2020-07-06 21:42:10 +0300

    Updates

---
+++ precache-manifest.2a2b1321b192bb071f8e1b19a3cae21a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d891210222f5dd2eda8344dffef70144",
+    "url": "/spacetravelers/index.html"
+  },
+  {
+    "revision": "5de30ada80ca13ec2241",
+    "url": "/spacetravelers/static/css/main.f5f9daef.chunk.css"
+  },
+  {
+    "revision": "b106e0c208da5b886510",
+    "url": "/spacetravelers/static/js/2.83b9f025.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spacetravelers/static/js/2.83b9f025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5de30ada80ca13ec2241",
+    "url": "/spacetravelers/static/js/main.c6d345f0.chunk.js"
+  },
+  {
+    "revision": "2b200301f868c3a596b4",
+    "url": "/spacetravelers/static/js/runtime-main.4ae74df8.js"
+  },
+  {
+    "revision": "eb8fbeeedd9afbb5797105fac8338a4c",
+    "url": "/spacetravelers/static/media/add.eb8fbeee.svg"
+  },
+  {
+    "revision": "dfc68ecb712ace41b2dbf4b114e41895",
+    "url": "/spacetravelers/static/media/arrow.dfc68ecb.svg"
+  },
+  {
+    "revision": "5f67fca3c1876276d43e0520e03b082e",
+    "url": "/spacetravelers/static/media/minus.5f67fca3.svg"
+  },
+  {
+    "revision": "6cfcdfa251af2be3b7be92a58d9beda8",
+    "url": "/spacetravelers/static/media/nothing.6cfcdfa2.png"
+  },
+  {
+    "revision": "20fe0f40b59cc4c4431bcab247fa0793",
+    "url": "/spacetravelers/static/media/spacetrav.20fe0f40.png"
+  }
+]);
